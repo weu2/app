4d6d60ad14696aa5a1c495613883d04de3efbd21
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-03-23 20:47:28 +1100

    More design stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>WeU</h1>
+        <div id="logodiv">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
