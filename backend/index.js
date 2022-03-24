c759c1d75c8491cd9463637981c01586d622640b
Caleb <waitcaleb@gmail.com>
2022-03-24 17:59:47 +1100

    serve static files in the backend

--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,10 @@ app.get('/api/v1/test', (req, res) => {
   res.send({ message : "Backend is responding" });
 });
 
+// serve the static files for the react app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
