1d56311f72b14a72cdc1c48c61b1d93f5f62e8ba
Caleb <waitcaleb@gmail.com>
2022-03-27 20:12:13 +1100

    see what happens

--- backend/src/common/jsondb.js
+++ backend/src/common/jsondb.js
@@ -2,41 +2,27 @@ const fs = require('fs');
 // probably a terrible idea
 
 class JSONDB {
-    constructor(filename) {
+    constructor(filepath) {
         //super();
-        this._filename = filename;
-        this._internal = [
-            {name:"Finish The report 1", due:new Date(), notes:"Some Notes 11", concern:"csci314"},
-            {name:"Finish The report 2", due:new Date(), notes:"Some Notes 12", concern:"csci314"},
-            {name:"Finish The report 3", due:new Date(), notes:"Some Notes 13", concern:"csci314"},
-            {name:"Finish The report 4", due:new Date(), notes:"Some Notes 14", concern:"csci314"},
-            {name:"Finish The report 5", due:new Date(), notes:"Some Notes 15", concern:"csci314"},
-            {name:"Finish The report 6", due:new Date(), notes:"Some Notes 16", concern:"csci314"},
-            {name:"Finish The report 7", due:new Date(), notes:"Some Notes 17", concern:"csci314"},
-            {name:"Finish The report 8", due:new Date(), notes:"Some Notes 18", concern:"csci314"},
-            {name:"Finish The report 9", due:new Date(), notes:"Some Notes 19", concern:"csci314"},
-            {name:"Finish The report 10", due:new Date(), notes:"Some Notes 11", concern:"csci314"},
-            {name:"Finish The report 11", due:new Date(), notes:"Some Notes 12", concern:"csci314"},
-            {name:"Finish The report 12", due:new Date(), notes:"Some Notes 13", concern:"csci314"},
-            {name:"Finish The report 13", due:new Date(), notes:"Some Notes 14", concern:"csci314"},
-            {name:"Finish The report 14", due:new Date(), notes:"Some Notes 15", concern:"csci314"},
-            {name:"Finish The report 15", due:new Date(), notes:"Some Notes 16", concern:"csci314"},
-            {name:"Finish The report 16", due:new Date(), notes:"Some Notes 17", concern:"csci314"},
-            {name:"Finish The report 17", due:new Date(), notes:"Some Notes 18", concern:"csci314"},
-            {name:"Finish The report 18", due:new Date(), notes:"Some Notes 191", concern:"csci314"},
-            {name:"Finish The report 19", due:new Date(), notes:"Some Notes 12", concern:"csci314"},
-            {name:"Finish The report 20", due:new Date(), notes:"Some Notes 13", concern:"csci314"},
-            {name:"Finish The report 21", due:new Date(), notes:"Some Notes 14", concern:"csci314"},
-            {name:"Finish The report 22", due:new Date(), notes:"Some Notes 15", concern:"csci314"}
-        ];
+        this._filepath = filepath;
+        this._internal = this.initialLoad();
     }
 
     // sync load the json file
-    load() {
+    initialLoad() {
+        try {
+            return JSON.parse(fs.readFileSync(this._filepath));
+        } catch(e) {
+            console.error(`${this._filepath} doesnt exist or the formatting is wrong`);
+            return [];
+        }
     }
 
     asyncUpdate() {
-        fs.writeFile(this._filename, JSON.stringify(this._internal));
+        fs.writeFile(this._filepath, JSON.stringify(this._internal, null, '\t'), (err) => {
+            if(err)
+                console.error(`${this._filepath} doesnt exist`);
+        });
     }
 
     find(key) {
@@ -45,11 +31,17 @@ class JSONDB {
             return keys.reduce((p, c) => {
                 return p && e[c] === key[c];
             }, true);
-        })
+        });
     }
 
-    update() {
-
+    update(key, val) {
+        const valkeys = Object.keys(val);
+        this.find(key).forEach(e => {
+            valkeys.forEach(k => {
+                e[k] = val[k];
+            });
+        });
+        this.asyncUpdate();
     }
 }
 
--- backend/src/v1/user.js
+++ backend/src/v1/user.js
@@ -9,9 +9,11 @@ router.use((req, res, next) => {
 
 router.post('/login', (req, res) => {
 
-    const db = new jsondb();
+    const db = new jsondb("data/users.json");
     console.log(db.find({notes:"Some Notes 11"}));
-
+    db.update({notes:"Some Notes 11"}, {concern:'fuck', name:'hello'});
+    //console.log(db.find({notes:"Some Notes 11"}));
+    
     console.log(req.body);
     res.send({message:'fuck off'});
 })
