5f3216a300fdcb006bd14c296fd5c4ebe2ee1043
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-03-28 22:51:30 +1100

    fiddle with backend auth script

--- backend/src/v1/auth.js
+++ backend/src/v1/auth.js
@@ -1,75 +1,74 @@
 // the auth file is for AUTHorisation and AUTHentication
-const jwt = require('../common/jwt');
-const JsonDB = require('../common/jsondb');
-const bcrypt = require('bcrypt');
+const jwt = require("../common/jwt");
+const JsonDB = require("../common/jsondb");
+const bcrypt = require("bcrypt");
 
 
 // these need to go in their own file for now they can live here
 // and synced as they change
-const jwtSecret = 'very good secret'; 
+const jwtSecret = "very good secret"; 
 let jwtID = 0; 
 
 function verifyClaim(claim) {
-    
+	// nah
 }
 
 function makeFakeUser(username, password) {
-    const users = new JsonDB('data/users.json');
-    bcrypt.hash(password, 10).then(hash => { // idk 10 rounds of salt?
-        users.add({
-            id:username,
-            pwdhash:hash
-        });
-    });
+	const users = new JsonDB("data/users.json");
+	bcrypt.hash(password, 10).then(hash => { // idk 10 rounds of salt?
+		users.add({
+			id: username,
+			pwdhash: hash
+		});
+	});
 }
 
-function createUser(username, password, iama, firstname, lastname) {
-    const users = new JsonDB('data/users.json');
-    bcrypt.hash(password, 10).then(hash => { // idk 10 rounds of salt?
-        users.add({
-            id:username,
-            pwdhash:hash,
-            iama: iama,
-            firstname: firstname,
-            lastname: lastname
-        });
-    });
+function createUser(username, password, category, firstname, lastname) {
+	const users = new JsonDB("data/users.json");
+	bcrypt.hash(password, 10).then(hash => { // idk 10 rounds of salt?
+		users.add({
+			id: username,
+			pwdhash: hash,
+			category: category,
+			firstname: firstname,
+			lastname: lastname
+		});
+	});
 }
 
 function createClaim(username) {
-    var iat = Math.floor(Date.now()/1000);
-    var exp = iat + (60*60); // + 1 hour
-    const payload = {
-        sub: username,
+	const iat = Math.floor(Date.now() / 1000);
+	const exp = iat + 3600; // + 1 hour
+	const payload = {
+		sub: username,
 		iat: iat,
-        exp: exp,
+		exp: exp,
 		jti: jwtID
-    };
-    jwtID++;
-    return jwt.createJWT(payload, jwtSecret);
+	};
+	jwtID++;
+	return jwt.createJWT(payload, jwtSecret);
 }
 
 function authenticate(username, password) {
-    const users = new JsonDB('data/users.json');
-    return new Promise((res, rej) => {    
-        const user = users.find({id:username});
-        if(user.length == 0) {
-            rej("No users match"); 
-            return;
-        }
-        bcrypt.compare(password, user[0].pwdhash, (err, result) => {
-            if(err) {
-                console.log(err);
-                // cant really give a specific since thats a securuity flaw 
-                rej("Authentication failed"); return;
-            }
-            if(result) {
-                res(createClaim(username));
-            } else {
-                rej("Authentication failed");
-            }
-        });
-    });
+	const users = new JsonDB("data/users.json");
+	return new Promise((res, rej) => {    
+		const user = users.find({id:username});
+		if (user.length === 0) {
+			rej("No users match"); 
+			return;
+		}
+		bcrypt.compare(password, user[0].pwdhash, (err, result) => {
+			if (err) {
+				console.log(err);
+				// cant really give a specific since thats a security flaw 
+				rej("Authentication failed");
+			} else if (result) {
+				res(createClaim(username));
+			} else {
+				rej("Authentication failed");
+			}
+		});
+	});
 }
 
 module.exports.verifyClaim = verifyClaim;
--- backend/src/v1/user.js
+++ backend/src/v1/user.js
@@ -1,32 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('./auth');
+const auth = require("./auth");
 
 router.use((req, res, next) => {
 	next();
 });
 
-router.post('/login', (req, res) => {
-    if(!req.body.email && !req.body.pwd)
-        res.send({status:'invalid api parameters'});
-    else 
-        auth.authenticate(req.body.email, req.body.pwd)
-            .then( claim => res.send({ status : 'ok' , claim : claim}))
-            .catch( r => res.send({ status : r }));
+router.post("/login", (req, res) => {
+	if (req.body.email && req.body.pwd) {
+		auth.authenticate(req.body.email, req.body.pwd)
+			.then(claim => res.send({ status : "ok" , claim : claim }))
+			.catch(r => res.send({ status : r }));
+	} else {
+		res.send({ status: "invalid api parameters" });
+	}
 })
 
-router.get('/maketestlogin', (req, res) => {
-    auth.makeFakeUser("test@example.com", "test");
-	res.send({ status : 'ok' });
+router.get("/maketestlogin", (req, res) => {
+	auth.makeFakeUser("test@example.com", "test");
+	res.send({ status: "ok" });
 });
 
-router.post('/register', (req, res) => {
-    if(!req.body.email && !req.body.pwd && !req.body.firstname && !req.body.lastname && !req.body.iama)
-        res.send({status:'invalid api parameters'});
-    else {
-        auth.createUser(req.body.email, req.body.pwd, req.body.firstname, req.body.lastname, req.body.iama);
-        res.send({ status : 'ok' });
-    }
+router.post("/register", (req, res) => {
+	if (req.body.email && req.body.pwd && req.body.category && req.body.firstname && req.body.lastname) {
+		auth.createUser(req.body.email, req.body.pwd, req.body.category, req.body.firstname, req.body.lastname);
+		res.send({ status: "ok" });
+	} else {
+		res.send({ status: "invalid api parameters" });
+	}
 });
 
 module.exports = router;
