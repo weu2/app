27b8cf365b711d1ed7b44b411e7d661983470620
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-03-29 01:39:15 +1100

    Refactor register page

--- frontend/src/Pages/RegisterTest.jsx
+++ frontend/src/Pages/RegisterTest.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faArrowRight, faFaceGrinWide, faLockOpen } from "@fortawesome/free-solid-svg-icons";
-import InputMask from "react-input-mask";
+import NumberFormat from "react-number-format";
 
 import "./RegisterTest.css";
 import recaptcha from "../Meme/recaptcha.png";
@@ -9,245 +9,264 @@ import wrong from "../Meme/wrong.png"
 import grid from "../Meme/grid.png"
 import rick from "../Meme/rick.gif"
 
-function clickSquare(e) {
-	e.preventDefault();
-	e.target.className = "Register-SquareActive";
-}
+class RegisterTest extends React.Component {
 
-function RegisterTest() {
-	const [page, setPage] = React.useState(0);
-	const [state, setState] = React.useState(null);
-	const [quizVisible, setQuizVisible] = React.useState(false);
-	
-	const customClick = (e) => {
+	constructor(props) {
+		super(props);
+		this.state = {
+			page: 0,
+			captchaState: null,
+			quizVisible: false,
+			quizVisibleTimeout: null,
+			quizDoneTimeout: null
+		};
+	}
+
+	componentDidMount() {
+		document.addEventListener("mousemove", this.updatePosition.bind(this), false);
+	}
+
+	componentWillUnmount() {
+		clearTimeout(this.state.quizVisibleTimeout);
+		clearTimeout(this.state.quizDoneTimeout);
+		document.removeEventListener("mousemove", this.updatePosition.bind(this));
+	}
+
+	updatePosition(e) {
+		if (!this.state.quizVisible) return;
+		const bounds = document.getElementById("Register-CaptchaQuiz-Table").getBoundingClientRect();
+		const boxWidth = bounds.width / 3;
+		const boxHeight = bounds.height / 3;
+		for (let i = 0; i < 9; i++) {
+			let x = bounds.left + (i % 3 * boxWidth) - e.clientX + (boxWidth * 0.5);
+			let y = bounds.top + (Math.floor(i / 3) * boxHeight) - e.clientY + (boxHeight * 0.5);
+			if (Math.sqrt(x * x + y * y) < boxWidth * 0.8) {
+				x = x > 0 ? boxWidth * 0.75 : -boxWidth * 0.75;
+				y = y > 0 ? boxHeight * 0.75 : -boxHeight * 0.75;
+			} else {
+				x = 0;
+				y = 0;
+			}
+			const square = document.getElementById(`Register-Square${i}`);
+			square.style.left = `${x}px`;
+			square.style.top = `${y}px`;
+		}
+	}
+
+	clickQuizSquare(e) {
 		e.preventDefault();
-		setPage(page + 1);
+		e.target.className = "Register-SquareActive";
+	}
+
+	showQuiz(e) {
+		e.preventDefault();
+		if (this.state.quizVisibleTimeout || this.state.captchaState) return;
+		this.setState({
+			captchaState: "spinning",
+			quizVisibleTimeout: setTimeout(() => {
+				this.setState({ quizVisible: true });
+			}, 500)
+		});
+	}
+
+	verifyQuiz(e) {
+		e.preventDefault();
+		if (this.state.quizDoneTimeout || this.state.captchaState !== "spinning") return;
+		this.setState({
+			captchaState: "waiting",
+			quizDoneTimeout: setTimeout(() => {
+				this.setState({ captchaState: "done" });
+			}, 500)
+		});
+	}
+
+	customClick(e) {
+		e.preventDefault();
+		this.setState({ page: this.state.page + 1 });
 	};
 
-	let timeout;
-	const toggleForm = (e) => {
-		e.preventDefault();
-		if (timeout || state) return;
-		setState("spinning");
-		timeout = setTimeout(() => {
-			setQuizVisible(true);
-		}, 500);
-	}
-
-	let timeout2;
-	const tickForm = (e) => {
-		e.preventDefault();
-		if (timeout2 || state !== "spinning") return;
-		setState("waiting");
-		timeout2 = setTimeout(() => {
-			setState("done");
-		}, 500);
-	}
-
-	React.useEffect(() => {
-		const updatePosition = (e) => {
-			if (!quizVisible) return;
-			const bounds = document.getElementById("Register-CaptchaQuiz-Table").getBoundingClientRect();
-			const boxWidth = bounds.width / 3;
-			const boxHeight = bounds.height / 3;
-			for (let i = 0; i < 9; i++) {
-				let x = bounds.left + (i % 3 * boxWidth) - e.clientX + (boxWidth * 0.5);
-				let y = bounds.top + (Math.floor(i / 3) * boxHeight) - e.clientY + (boxHeight * 0.5);
-				if (Math.sqrt(x * x + y * y) < boxWidth * 0.8) {
-					x = x > 0 ? boxWidth * 0.75 : -boxWidth * 0.75;
-					y = y > 0 ? boxHeight * 0.75 : -boxHeight * 0.75;
-				} else {
-					x = 0;
-					y = 0;
-				}
-				const square = document.getElementById(`Register-Square${i}`);
-				square.style.left = `${x}px`;
-				square.style.top = `${y}px`;
-			}
-		}
-		document.addEventListener("mousemove", updatePosition, false);
-		return () => {
-			document.removeEventListener("mousemove", updatePosition);
-		}
-	}, [quizVisible]);
-
-	return (
-		<div className="Register">
-			<div className="Register-Content">
-				<h1>Register</h1>
-				{
-					(page > 0) ?
-					<button className="btn Register-BackButton" onClick={() => setPage(page - 1)}>
-						<FontAwesomeIcon icon={faChevronLeft} /> Back
-					</button> :
-					null
-				}
-				<div className={page === 0 ? null : "Register-Page-Offscreen"}>
-					<h2>I am a...</h2>
-					<button onClick={customClick} className="btn btn-primary btn-bevel Register-CategoryButton">
-						Customer<FontAwesomeIcon icon={faArrowRight} />
-					</button>
-					<button onClick={customClick} className="btn btn-primary btn-bevel Register-CategoryButton">
-						Service Professional<FontAwesomeIcon icon={faArrowRight} />
-					</button>
-				</div>
-				<form onSubmit={customClick}>
-					<div className={page === 1 ? null : "Register-Page-Offscreen"}>
-						<div>
-							<label htmlFor="firstName">First Name</label>
-							<input
-								className="form-input"
-								type="text"
-								required={page === 1}
-								placeholder="Dave"
-								autoComplete="given-name"
-							/>
-						</div>
-						<div>
-							<label htmlFor="lastName">Last Name</label>
-							<input
-								className="form-input"
-								type="text"
-								required={page === 1}
-								placeholder="O"
-								autoComplete="family-name"
-							/>
-						</div>
-						<div>
-							<label htmlFor="email">Email</label>
-							<input
-								className="form-input"
-								type="email"
-								required={page === 1}
-								placeholder="davo@gmail.com"
-								autoComplete="email"
-							/>
-						</div>
-						<div>
-							<label htmlFor="password">Password</label>
-							<input
-								className="form-input"
-								type="password"
-								required={page === 1}
-								autoComplete="current-password"
-							/>
-						</div>
+	render() {
+		return (
+			<div className="Register">
+				<div className="Register-Content">
+					<h1>Register</h1>
+					{
+						(this.state.page > 0) ?
+						<button className="btn Register-BackButton" onClick={() => this.setState({ page: this.state.page - 1 })}>
+							<FontAwesomeIcon icon={faChevronLeft} /> Back
+						</button>
+						: null
+					}
+					<div className={this.state.page === 0 ? null : "Register-Page-Offscreen"}>
+						<h2>I am a...</h2>
+						<button onClick={this.customClick.bind(this)} className="btn btn-primary btn-bevel Register-CategoryButton">
+							Customer<FontAwesomeIcon icon={faArrowRight} />
+						</button>
+						<button onClick={this.customClick.bind(this)} className="btn btn-primary btn-bevel Register-CategoryButton">
+							Service Professional<FontAwesomeIcon icon={faArrowRight} />
+						</button>
 					</div>
-					<div className={page === 2 ? null : "Register-Page-Offscreen"}>
-						<label htmlFor="cardNumber">Mother's credit card number</label>
-						<div className="Register-CreditCard">
-							<div className="Register-CardStrip" />
-							<div className="Register-CardInputs">
-								<div className="Register-CardNumberContainer">
-									<InputMask
-										className="form-input"
-										id="Register-CardNumber"
-										mask="9999-9999-9999-9999"
-										placeholder="xxxx-xxxx-xxxx-xxxx"
-										required={page === 2}
-										autoComplete="cc-number"
-									/>
-									<FontAwesomeIcon className="Register-CardLock" icon={faLockOpen} />
-								</div>
-								<div className="Register-CardSecrets">
-									<InputMask
-										className="form-input"
-										id="Register-CardDate"
-										mask="99/99"
-										placeholder="MM/YY"
-										required={page === 2}
-										autoComplete="cc-exp"
-									/>
-									<InputMask
-										className="form-input"
-										id="Register-CardSecret"
-										mask="999"
-										placeholder="CVC"
-										required={page === 2}
-										autoComplete="cc-csc"
-									/>
+					<form onSubmit={this.customClick.bind(this)}>
+						<div className={this.state.page === 1 ? null : "Register-Page-Offscreen"}>
+							<div>
+								<label htmlFor="firstName">First Name</label>
+								<input
+									className="form-input"
+									type="text"
+									required={this.state.page === 1}
+									placeholder="Dave"
+									autoComplete="given-name"
+								/>
+							</div>
+							<div>
+								<label htmlFor="lastName">Last Name</label>
+								<input
+									className="form-input"
+									type="text"
+									required={this.state.page === 1}
+									placeholder="O"
+									autoComplete="family-name"
+								/>
+							</div>
+							<div>
+								<label htmlFor="email">Email</label>
+								<input
+									className="form-input"
+									type="email"
+									required={this.state.page === 1}
+									placeholder="davo@gmail.com"
+									autoComplete="email"
+								/>
+							</div>
+							<div>
+								<label htmlFor="password">Password</label>
+								<input
+									className="form-input"
+									type="password"
+									required={this.state.page === 1}
+									autoComplete="current-password"
+								/>
+							</div>
+						</div>
+						<div className={this.state.page === 2 ? null : "Register-Page-Offscreen"}>
+							<label htmlFor="cardNumber">Mother's credit card number</label>
+							<div className="Register-CreditCard">
+								<div className="Register-CardStrip" />
+								<div className="Register-CardInputs">
+									<div className="Register-CardNumberContainer">
+										<NumberFormat
+											className="form-input"
+											id="Register-CardNumber"
+											format="####-####-####-####"
+											mask="_"
+											placeholder="xxxx-xxxx-xxxx-xxxx"
+											required={this.state.page === 2}
+											autoComplete="cc-number"
+										/>
+										<FontAwesomeIcon className="Register-CardLock" icon={faLockOpen} />
+									</div>
+									<div className="Register-CardSecrets">
+										<NumberFormat
+											className="form-input"
+											id="Register-CardDate"
+											format="##/##"
+											placeholder="MM/YY"
+											mask="_"
+											required={this.state.page === 2}
+											autoComplete="cc-exp"
+										/>
+										<NumberFormat
+											className="form-input"
+											id="Register-CardSecret"
+											format="###"
+											mask="_"
+											placeholder="CVC"
+											required={this.state.page === 2}
+											autoComplete="cc-csc"
+										/>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<div className={page === 3 ? null : "Register-Page-Offscreen"}>
-						<div className="Register-Captcha" onClick={toggleForm}>
+						<div className={this.state.page === 3 ? null : "Register-Page-Offscreen"}>
+							<div className="Register-Captcha" onClick={this.showQuiz.bind(this)}>
+								{
+									(this.state.captchaState === "spinning") ?
+									<div className="Register-Captcha-Spinner" /> :
+									(
+										(this.state.captchaState === "waiting" || this.state.captchaState === "done") ?
+										<div className="Register-Captcha-Wrong" style={{backgroundImage: `url(${wrong})`}}/> :
+										<div className="Register-Captcha-Checkbox" />
+									)
+								}
+								<span>{this.state.captchaState === "done" ? "WRONG" : "I'm a robot"}</span>
+								<img src={recaptcha} className="Register-Captcha-Icon" alt="captcha" />
+							</div>
 							{
-								(state === "spinning") ?
-								<div className="Register-Captcha-Spinner" /> :
-								(
-									(state === "waiting" || state === "done") ?
-									<div className="Register-Captcha-Wrong" style={{backgroundImage: `url(${wrong})`}}/> :
-									<div className="Register-Captcha-Checkbox" />
-								)
+								this.state.quizVisible ?
+								<div className="Register-CaptchaQuiz">
+									<div className="Register-CaptchaQuiz-Header">{
+										(this.state.captchaState === "done") ?
+										<div>Your credit card has been charged <FontAwesomeIcon icon={faFaceGrinWide} /></div> :
+										"Select all images with good teaching practice."
+									}</div>
+									<table id="Register-CaptchaQuiz-Table">
+										<tbody>
+											<tr>
+												<td><div id="Register-Square0" alt="square0" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+												<td><div id="Register-Square1" alt="square1" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+												<td><div id="Register-Square2" alt="square2" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+											</tr>
+											<tr>
+												<td><div id="Register-Square3" alt="square3" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+												<td><div id="Register-Square4" alt="square4" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+												<td><div id="Register-Square5" alt="square5" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+											</tr>
+											<tr>
+												<td><div id="Register-Square6" alt="square6" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+												<td><div id="Register-Square7" alt="square7" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+												<td><div id="Register-Square8" alt="square8" style={{
+													backgroundImage: `url(${this.state.captchaState === "done" ? rick : grid})`
+												}} onMouseDown={this.clickQuizSquare} /></td>
+											</tr>
+										</tbody>
+									</table>
+									{
+										(this.state.captchaState === "done") ? null :
+										<button className="btn btn-primary Register-CaptchaQuiz-Verify" onClick={this.verifyQuiz.bind(this)}>Verify</button>
+									}
+								</div>
+								: null
 							}
-							<span>{state === "done" ? "WRONG" : "I'm a robot"}</span>
-							<img src={recaptcha} className="Register-Captcha-Icon" alt="captcha" />
 						</div>
 						{
-							quizVisible ?
-							<div className="Register-CaptchaQuiz">
-								<div className="Register-CaptchaQuiz-Header">{
-									(state === "done") ?
-									<div>Your credit card has been charged <FontAwesomeIcon icon={faFaceGrinWide} /></div> :
-									"Select all images with good teaching practice."
-								}</div>
-								<table id="Register-CaptchaQuiz-Table">
-									<tbody>
-										<tr>
-											<td><div id="Register-Square0" alt="square0" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-											<td><div id="Register-Square1" alt="square1" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-											<td><div id="Register-Square2" alt="square2" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-										</tr>
-										<tr>
-											<td><div id="Register-Square3" alt="square3" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-											<td><div id="Register-Square4" alt="square4" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-											<td><div id="Register-Square5" alt="square5" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-										</tr>
-										<tr>
-											<td><div id="Register-Square6" alt="square6" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-											<td><div id="Register-Square7" alt="square7" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-											<td><div id="Register-Square8" alt="square8" style={{
-												backgroundImage: `url(${state === "done" ? rick : grid})`
-											}} onMouseDown={clickSquare} /></td>
-										</tr>
-									</tbody>
-								</table>
-								{
-									(state === "done") ?
-									null :
-									<button className="btn btn-primary Register-CaptchaQuiz-Verify" onClick={tickForm}>Verify</button>
-								}
-							</div>
+							(this.state.page >= 1 && this.state.page <= 2) ?
+							<button className="btn btn-primary btn-bevel Register-NextButton" type="submit">
+								Next<FontAwesomeIcon icon={faArrowRight} />
+							</button>
 							: null
 						}
-					</div>
-					{
-						(page >= 1 && page <= 2) ?
-						<button className="btn btn-primary btn-bevel Register-NextButton" type="submit">
-							Next<FontAwesomeIcon icon={faArrowRight} />
-						</button> : null
-					}
-				</form>
+					</form>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default RegisterTest;
