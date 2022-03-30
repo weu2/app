fbfd348eaee5250185cb1cddec1020a910227f1d
Caleb <waitcaleb@gmail.com>
2022-03-30 13:39:03 +1100

    maybe https

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,4 +18,29 @@ const static = require('./static');
 app.use('/', static);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+if(!process.env.PORT) {
+
+}
+
+try {
+    const fs = require('fs');
+    const https = require('https');
+    const key = fs.readFileSync('./certs/selfsigned.pem');
+    const cert = fs.readFileSync('./certs/selfsigned.cert');
+    const options = {
+        key: key,
+        cert: cert
+    };
+    const server = https.createServer(options, app)
+    server.listen(port, () => console.log(`Listening on port ${port}`));
+} catch(e) {
+    console.error('Please either run the build command to generate tls certificate or install them to the correct directory');
+    console.error('If you are hosting the server locally for development you can ignore this.');
+    console.error('Starting http server...');
+    const http = require('http');
+    const server = http.createServer(app)
+    server.listen(port, () => console.log(`Listening on port ${port}`));
+}
+
+
--- build.bat
+++ build.bat
@@ -1,5 +1,13 @@
 @echo off
 
 pushd frontend
-call npm run build
+    call npm run build
+popd
+
+pushd backend
+    mkdir certs
+popd
+
+pushd backend\certs
+    openssl req -nodes -new -x509 -keyout selfsigned.pem -out selfsigned.cert
 popd
