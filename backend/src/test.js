f11c9be3f6b08cf0107810d2d38c045e023f48f8
Caleb <waitcaleb@gmail.com>
2022-04-27 20:29:13 +1000

    this should fail

--- backend/src/test.js
+++ backend/src/test.js
@@ -6,6 +6,6 @@ describe("/test endpoint", () => {
     it("should return a response", async () => {
         const response = await request.get("/api/v1/test");
         expect(response.status).toBe(200)
-        expect(response.text).toBe("{\"jwtverify\":true}");
+        expect(response.text).toBe("{\"jwtverify\":    true}");
     });
 });
