0b1532a439bd4aac8fb99495b969d5f758ba62ef
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-05-03 22:09:44 +1000

    add missing semicolons js moment

--- frontend/src/Tests/Register.test.jsx
+++ frontend/src/Tests/Register.test.jsx
@@ -13,13 +13,13 @@ it("checks register input names match backend names", () => {
 	// simulate customer button click
 	screen.getByText("Customer").click();
 
-	expect(screen.getByLabelText("Email").name).toEqual("email")
-	expect(screen.getByLabelText("Password").name).toEqual("password")
-	expect(screen.getByLabelText("First Name").name).toEqual("firstName")
-	expect(screen.getByLabelText("Last Name").name).toEqual("lastName")
-	expect(screen.getByLabelText("Address").name).toEqual("address")
-	expect(screen.getByLabelText("Phone Number").name).toEqual("phoneNumber")
-	expect(screen.getByLabelText("License Number").name).toEqual("license")
+	expect(screen.getByLabelText("Email").name).toEqual("email");
+	expect(screen.getByLabelText("Password").name).toEqual("password");
+	expect(screen.getByLabelText("First Name").name).toEqual("firstName");
+	expect(screen.getByLabelText("Last Name").name).toEqual("lastName");
+	expect(screen.getByLabelText("Address").name).toEqual("address");
+	expect(screen.getByLabelText("Phone Number").name).toEqual("phoneNumber");
+	expect(screen.getByLabelText("License Number").name).toEqual("license");
 });
 
 it("checks register input types are valid", () => {
@@ -27,13 +27,13 @@ it("checks register input types are valid", () => {
 	// simulate customer button click
 	screen.getByText("Customer").click();
 	
-	expect(screen.getByLabelText("Email").type).toEqual("email")
-	expect(screen.getByLabelText("Password").type).toEqual("password")
-	expect(screen.getByLabelText("First Name").type).toEqual("text")
-	expect(screen.getByLabelText("Last Name").type).toEqual("text")
-	expect(screen.getByLabelText("Address").type).toEqual("text")
-	expect(screen.getByLabelText("Phone Number").type).toEqual("tel")
-	expect(screen.getByLabelText("License Number").type).toEqual("number")
+	expect(screen.getByLabelText("Email").type).toEqual("email");
+	expect(screen.getByLabelText("Password").type).toEqual("password");
+	expect(screen.getByLabelText("First Name").type).toEqual("text");
+	expect(screen.getByLabelText("Last Name").type).toEqual("text");
+	expect(screen.getByLabelText("Address").type).toEqual("text");
+	expect(screen.getByLabelText("Phone Number").type).toEqual("tel");
+	expect(screen.getByLabelText("License Number").type).toEqual("number");
 });
 
 it("checks register inputs are required", () => {
@@ -41,11 +41,11 @@ it("checks register inputs are required", () => {
 	// simulate customer button click
 	screen.getByText("Customer").click();
 	
-	expect(screen.getByLabelText("Email").required).toEqual(true)
-	expect(screen.getByLabelText("Password").required).toEqual(true)
-	expect(screen.getByLabelText("First Name").required).toEqual(true)
-	expect(screen.getByLabelText("Last Name").required).toEqual(true)
-	expect(screen.getByLabelText("Address").required).toEqual(true)
-	expect(screen.getByLabelText("Phone Number").required).toEqual(true)
-	expect(screen.getByLabelText("License Number").required).toEqual(true)
+	expect(screen.getByLabelText("Email").required).toEqual(true);
+	expect(screen.getByLabelText("Password").required).toEqual(true);
+	expect(screen.getByLabelText("First Name").required).toEqual(true);
+	expect(screen.getByLabelText("Last Name").required).toEqual(true);
+	expect(screen.getByLabelText("Address").required).toEqual(true);
+	expect(screen.getByLabelText("Phone Number").required).toEqual(true);
+	expect(screen.getByLabelText("License Number").required).toEqual(true);
 });
