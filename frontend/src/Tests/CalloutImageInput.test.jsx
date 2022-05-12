50902fdd8b76cec45f495681f0a064fb4df7bf66
Caleb <waitcaleb@gmail.com>
2022-05-12 17:20:36 +1000

    fix callout image test

--- frontend/src/Tests/CalloutImageInput.test.jsx
+++ frontend/src/Tests/CalloutImageInput.test.jsx
@@ -3,13 +3,13 @@ import { render } from "@testing-library/react";
 import CalloutImageInput from "../Components/CalloutImageInput";
 
 it("checks callout image input names match backend names", () => {
-	const { container } = render(<CalloutImageInput callout={{ images: [] }}/>);
+	const { container } = render(<CalloutImageInput callout={{ images: [] }} userType={"CUSTOMER"}/>);
 	expect(container.querySelector("input[name=\"image\"]")).toBeInTheDocument();
 	expect(container.querySelector("input[name=\"calloutId\"]")).toBeInTheDocument();
 });
 
 it("checks callout image input types are valid", () => {
-	const { container } = render(<CalloutImageInput callout={{ images: [] }}/>);
+	const { container } = render(<CalloutImageInput callout={{ images: [] }} userType={"CUSTOMER"}/>);
 	expect(container.querySelector("input[name=\"image\"]").type).toBe("file");
 	expect(container.querySelector("input[name=\"calloutId\"]").type).toBe("hidden");
 });
