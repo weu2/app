2321c9eda2d85ef36858af18527d6d7a17f2098e
Caleb <waitcaleb@gmail.com>
2022-05-17 23:24:27 +1000

    redirect http to https

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,28 @@
 
-const port = process.env.PORT || 5000;
-
+const httpsport = process.env.HTTPSPORT || 5000;
+const httpport = process.env.HTTPPORT || 5001;
+const key = process.env.TLSKEY || './misc/tls/local.key';
+const cert = process.env.TLSCERT || './misc/tls/local.pem';
 try {
     const fs = require('fs');
     const https = require('https');
-    const key = fs.readFileSync('./misc/tls/local.key');
-    const cert = fs.readFileSync('./misc/tls/local.pem');
+    const key = fs.readFileSync(key);
+    const cert = fs.readFileSync(cert);
     const options = {
         key: key,
         cert: cert
     };
     const app = require('./app.js');
-    const server = https.createServer(options, app)
-    server.listen(port, () => console.log(`HTTPS server started, listening on port ${port}`));
-} catch(e) {
+    const secureserver = https.createServer(options, app)
+    secureserver.listen(port, () => console.log(`HTTPS server started, listening on port ${httpsport}`));
+
     console.error('TLS certificate not present');
     console.log(e)
-    // const http = require('http');
-    // const server = http.createServer(app)
-    // server.listen(port, () => console.log(`Listening on port ${port}`));
+    const http = require('http');
+    const httpServer = require('./httpServer.js');
+    const notsecureserver = http.createServer(httpServer)
+    notsecureserver.listen(port, () => console.log(`Listening on port ${httpport}`));
+
+} catch(e) {
+    console.log(e);
 } 
